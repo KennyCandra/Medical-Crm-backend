c5477fdccb9b47fa5026bd8a455244022513debf
KennyCandra <142558663+KennyCandra@users.noreply.github.com>
2025-06-28 22:40:06 +0300

    new host

--- app.ts
+++ app.ts
@@ -24,7 +24,7 @@ app.use(cookiesParser())
 
 
 app.use(cors({
-    origin: ['https://medical-crm-fronted.vercel.app', 'http://localhost:5173'],
+    origin: ['https://lambent-alfajores-c5c459.netlify.app', 'http://localhost:5173'],
     credentials: true,
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'Authorization']
